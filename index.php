9fdabd61b7b96196a8f236e49f460d1f848dcf5c
Greg Martin <gremartin@yahoo.com>
2013-09-16 23:04:10 -0400

    Updating Index page with Bio and basic html structure

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-Welcome to P1
+<html>
+<head>
+<title>Project 1 for dwa15</title>
+<style type="text/css">
+
+</style>
+</head>
+<body>
+<h1>Greg Martin</h1>
+<div id="mainText">
+<p>I am originally from Thomaston, CT.  I graduated from Boston College
+ with a degree in Music and currently work at MCPHS University (formerly the Massachusetts 
+ College of Pharmacy and Health Sciences) in Boston.  I earned an undergraduate certificate
+ in software development from Boston University's Metropolitan College in 2010 and started
+ studying Web Design last fall in MassArt's Continuing Education program.</p>
+<p>In my spare time, I play double bass with several jazz groups and the Brookline Symphony Orchestra, 
+and guitar whenever I can.</p>
+<p>For this class I will be coding in Notepad++</p>
+</div>
+</body>
+
+</html>
