64df8ccb1c5bb38637f9ae4d91740aaf3638b03e
Greg Martin <gremartin@yahoo.com>
2013-09-17 21:30:31 -0400

    Added doctype

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
@@ -5,18 +6,18 @@
 <link href="p1.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
-<div id= "myName">
-	<h1>Greg Martin</h1>
-</div>
-<div id= "mainText">
-	<p>I am originally from Thomaston, CT.  I graduated from Boston College
-	 with a degree in Music and currently work at MCPHS University (formerly the Massachusetts 
-	 College of Pharmacy and Health Sciences) in Boston.  I earned an undergraduate certificate
-	 in software development from Boston University's Metropolitan College in 2010 and started
-	 studying Web Design last fall in MassArt's Continuing Education program.</p>
-	<p>In my spare time, I play double bass with several jazz groups and the Brookline Symphony Orchestra, 
-	and guitar whenever I can.</p>
-	<p>For this class, I will be coding in Notepad++</p>
-</div>
+	<div id= "myName">
+		<h1>Greg Martin</h1>
+	</div>
+	<div id= "mainText">
+		<p>I am originally from Thomaston, CT.  I graduated from Boston College
+		 with a degree in Music and currently work at MCPHS University (formerly the Massachusetts 
+		 College of Pharmacy and Health Sciences) in Boston.  I earned an undergraduate certificate
+		 in software development from Boston University's Metropolitan College in 2010 and started
+		 studying Web Design last fall in MassArt's Continuing Education program.</p>
+		<p>In my spare time, I play double bass with several jazz groups and the Brookline Symphony Orchestra, 
+		and guitar whenever I can.</p>
+		<p>For this class, I will be coding in Notepad++</p>
+	</div>
 </body>
 </html>
