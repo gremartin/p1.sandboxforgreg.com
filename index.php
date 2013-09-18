47004cea4ac5a79e6585963ad5b01845e3f937c7
Greg Martin <gremartin@yahoo.com>
2013-09-17 20:39:35 -0400

    Fixed charset problem and cleaned up code a little

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-@charset "utf-8";
 <html>
 <head>
+<meta charset="utf-8">
 <title>Project 1 for dwa15</title>
 <style type="text/css">
 body{
-	background-color:rgb(204,204,204);
+	background-color: rgb(204,204,204);
 	}
 #myName{
 	background-color: rgb(51, 153, 204);
@@ -18,28 +18,27 @@ h1{
 #mainText{
 	width: 590;
 	margin: 20px 80px 0px 80px ;
-	text-align:center;
+	text-align: center;
 	}
 p{
-	font-family: "Times New Roman", Georgia, serif;
+	font-family: "Times New Roman",Georgia,serif;
 	}
 }
 </style>
 </head>
 <body>
 <div id ="myName">
-<h1>Greg Martin</h1>
+	<h1>Greg Martin</h1>
 </div>
 <div id="mainText">
-<p>I am originally from Thomaston, CT.  I graduated from Boston College
- with a degree in Music and currently work at MCPHS University (formerly the Massachusetts 
- College of Pharmacy and Health Sciences) in Boston.  I earned an undergraduate certificate
- in software development from Boston University's Metropolitan College in 2010 and started
- studying Web Design last fall in MassArt's Continuing Education program.</p>
-<p>In my spare time, I play double bass with several jazz groups and the Brookline Symphony Orchestra, 
-and guitar whenever I can.</p>
-<p>For this class, I will be coding in Notepad++</p>
+	<p>I am originally from Thomaston, CT.  I graduated from Boston College
+	 with a degree in Music and currently work at MCPHS University (formerly the Massachusetts 
+	 College of Pharmacy and Health Sciences) in Boston.  I earned an undergraduate certificate
+	 in software development from Boston University's Metropolitan College in 2010 and started
+	 studying Web Design last fall in MassArt's Continuing Education program.</p>
+	<p>In my spare time, I play double bass with several jazz groups and the Brookline Symphony Orchestra, 
+	and guitar whenever I can.</p>
+	<p>For this class, I will be coding in Notepad++</p>
 </div>
 </body>
-
 </html>
